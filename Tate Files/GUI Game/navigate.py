0b0b05fc353f46ce397d3a25e538db87d9f565bb
tate-awesome <tate.abercrombie2017@gmail.com>
2025-12-16 14:05:50 -0700

    Scanning modbus values nicely

--- Tate Files/GUI Game/navigate.py
+++ Tate Files/GUI Game/navigate.py
@@ -218,7 +218,7 @@ class Navigate:
 
         sniff_button = place.big_button(middle_pane, "Start Sniffing")
         def start_sniff():
-            spoof_button.configure(command=start_spoof, text="Sniffing...")
+            sniff_button.configure(command=start_spoof, text="Sniffing...")
             net.packet_sniffing.start()
         sniff_button.configure(command=start_sniff)
         
