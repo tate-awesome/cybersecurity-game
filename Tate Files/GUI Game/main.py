8b5d4b222e2601633f50cc2648d8d234808a539e
tate-awesome <tate.abercrombie2017@gmail.com>
2025-12-16 10:45:13 -0700

    Added ICS attacks

--- Tate Files/GUI Game/main.py
+++ Tate Files/GUI Game/main.py
@@ -13,8 +13,9 @@ root.geometry(f"{int(screen_width*3/4)}x{int(screen_height*3/4)}")
 # app.state("zoomed")
 root.title("Cybersecurity Game")
 
+# nav = navigate.Navigate(root)
 nav = navigate.Navigate(root)
-nav.hacker_final()
+nav.hacker_hardware_demo()
 # nav.hacker_start()
 
 
