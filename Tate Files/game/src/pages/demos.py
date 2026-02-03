30196876f24f522229e688971b12f74db97e14d3
tate-awesome <tate.abercrombie2017@gmail.com>
2026-02-02 22:59:41 -0700

    Added reversible button to common

--- Tate Files/game/src/pages/demos.py
+++ Tate Files/game/src/pages/demos.py
@@ -1,10 +1,33 @@
 from ..widgets.common import Common as place
 from ..widgets.map import Map as map
 
+from ..network import network_controller
+
 class Demos:
 
+
     def map(root):
+
+        # Create and define network control
+        net = network_controller.HardwareNetwork()
+
+        def start_attack():
+            net.start_arp()
+            net.start_nfq()
+
+        def stop_attack():
+            net.stop_nfq()
+            net.stop_arp()
+        
+        # Build page
         menu_bar = place.menu_bar(root, "Demo")
-        place.menu_bar_button(menu_bar, "Button1", None)
+        
+        no_button = place.menu_bar_button(menu_bar, "Start Printing")
+        place.configure_reversible_button(no_button, lambda:print("start"), lambda:print("stop"), "Printing")
+        
+        attack_button = place.menu_bar_button(menu_bar, "Start Attack")
+        place.configure_reversible_button(attack_button, start_attack, stop_attack, "Attack")
+
         place.trifold(root)
+
         return
