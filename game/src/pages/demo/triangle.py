d969adc260ac260573c4844f78864e296657ee61
Tate Abercrombie <tate.aber.4@example.com>
2026-02-06 15:26:32 -0700

    Add other pages to router

---
+++ game/src/pages/demo/triangle.py
@@ -0,0 +1,16 @@
+from ...app_core.context import Context
+from customtkinter import CTkCanvas
+from threading import Lock
+from ...widgets.map import Map
+
+class Triangle:
+    def __init__(self, context: Context):       
+
+        world_map = Map(context.root, self.draw_test_plane, 100)
+
+    def draw_test_plane(self, canvas: CTkCanvas, draw_lock: Lock, scale: float, offset: tuple[float, float]):
+        from ...drawing.viewport import ViewPort
+        draw = ViewPort(canvas, scale, offset)
+        with draw_lock:
+            canvas.delete("all")
+            draw.test_triangle()
