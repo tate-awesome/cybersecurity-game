0d2f389b5997c28c65908dee8f39cd1b74e30e95
tate-awesome <tate.abercrombie2017@gmail.com>
2026-02-11 14:58:31 -0700

    Add the original proof of concept as playable demo

--- game/src/pages/title/select_demo.py
+++ game/src/pages/title/select_demo.py
@@ -1,6 +1,7 @@
 from ...app_core.context import Context
 from ...widgets.style import Style
 from ...widgets import title
+from ..demo.v0.main import run
 
 
 class SelectDemo:
@@ -17,5 +18,6 @@ class SelectDemo:
         title.button(style, bw, "Saved Map", lambda:router.show("demo/saved_map"))
         title.button(style, bw, "Sprites", lambda:router.show("demo/sprites"))
         title.button(style, bw, "Triangle", lambda:router.show("demo/triangle"))
+        title.button(style, bw, "Proof of Concept", run)
         title.button(style, bw, "Back", lambda:router.show("title/select_mode"))
         title.button(style, bw, "Quit", router.quit)
