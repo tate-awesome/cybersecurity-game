6a8381b80726ecdc220e657e6488bf5fb42049a6
tate-awesome <tate.abercrombie2017@gmail.com>
2026-02-11 11:16:47 -0700

    Add Style class to track styling options

---
+++ game/src/widgets/style.py
@@ -0,0 +1,26 @@
+from customtkinter import CTkFont
+
+class Style:
+
+    def __init__(self, ui_scale: float=100.0):
+        self.scale = ui_scale
+        self.GAP = 10
+        self.PANE_MIN = self.GAP*4
+        self.fonts = {}
+        # DATA_FONT = CTkFont(family="Courier", size=16)
+# HEADER_FONT = CTkFont(family="Arial", size=24)
+# TITLE_FONT = CTkFont(family="Arial", size=max(32, root.winfo_height()//5), weight="bold")
+
+
+    def get_font(self, name="default"):
+        if name not in self.fonts:
+            if name == "default":
+                size = int(16.0*self.scale/100.0)
+                self.fonts[name] = CTkFont(family="Arial", size=size)
+            elif name == "subtitle":
+                size=int(18.0*self.scale/100.0)
+                self.fonts[name] = CTkFont(size=size)
+            else:
+                size = int(14.0*self.scale/100.0)
+                self.fonts[name] = CTkFont(size=size)
+        return self.fonts[name]
