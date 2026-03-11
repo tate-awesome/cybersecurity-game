17347f2fe0a423854324912c8a956c1bdebbe844
Tate Abercrombie <tate.abercrombie2017@gmail.com>
2026-03-11 17:46:52 -0600

    Fix NMap and MetaPacket for other interfaces

--- game/src/network/hardware/nmap.py
+++ game/src/network/hardware/nmap.py
@@ -2,7 +2,7 @@
 Module
 '''
 import scapy.all as scapy
-from scapy.all import Packet
+from scapy.all import Packet, ARP
 import ipaddress, netifaces
 from ..data_buffer import DataBuffer
 
@@ -13,10 +13,13 @@ class NMapper:
     def do_nmap(self):
         # TODO nmap console command outputs
         # TODO Name these things correctly
-        ip = self.get_ip()
+        iface = str(self.get_interface())
+        self.buffer.put("nmap", "Detected interface", ["Your network interface", iface])
+
+        ip = self.get_ip(iface)
         self.buffer.put("nmap", "info", ["Your IP Address", ip])
 
-        netmask = self.get_netmask()
+        netmask = self.get_netmask(iface)
         self.buffer.put("nmap", "info", ["Network Mask", netmask])
 
         network = self.compute_network(ip, netmask)
@@ -24,21 +27,33 @@ class NMapper:
 
         ping_packet, answered, unanswered = self.ping_hosts(network)
         self.buffer.put("nmap", "ARP Probe", ping_packet)
+
+        responses = []
         
         for received in answered:
             self.buffer.put("nmap", "Answered ARP Request", received[0])
             self.buffer.put("nmap", "ARP Response", received[1])
+            responses.append(received[1])
+
+        hosts = self.compute_hosts(responses)
+        for host in hosts:
+            self.buffer.put("nmap", "info", ["Found host", host])
 
 
 
-    def get_ip(self) -> str:
-        iface = "wlp0s20f3"
+
+
+    def get_interface(self):
+        interface = scapy.conf.iface
+        return interface
+
+
+    def get_ip(self, iface) -> str:
         info = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]
         ip = info["addr"]
         return str(ip)
 
-    def get_netmask(self, ) -> str:
-        iface = "wlp0s20f3"
+    def get_netmask(self, iface) -> str:
         info = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]
         netmask = info["netmask"]
         return str(netmask)
@@ -47,12 +62,18 @@ class NMapper:
         network = ipaddress.IPv4Network(f"{ip}/{netmask}", strict=False)
         return str(network)
 
-    def ping_hosts(self, network: str, iface="wlp0s20f3") -> tuple[Packet, list, list]:
+    def ping_hosts(self, network: str) -> tuple[Packet, list, list]:
         '''
         May block for up to 2 seconds.
         '''
         network = str(network)
         ping_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") / scapy.ARP(pdst=network)
-        answered, unanswered = scapy.srp(ping_packet, timeout=2, verbose=False)
+        answered, unanswered = scapy.srp(ping_packet, timeout=2.0, verbose=False)
 
         return ping_packet, answered, unanswered
+    
+    def compute_hosts(self, responses: list[Packet]):
+        infos = []
+        for pkt in responses:
+            infos.append(f"Host IP {pkt[ARP].psrc} is at MAC address {pkt[ARP].hwsrc}")
+        return infos
