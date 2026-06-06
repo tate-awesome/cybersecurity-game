4799803772ae64d307578454754e637d3de0ae36
Tate Abercrombie <tate.aber.4@example.com>
2026-06-06 00:59:32 -0600

    Add the right directory for pcap loading

--- game/src/network/saved/loader.py
+++ game/src/network/saved/loader.py
@@ -1,6 +1,8 @@
 from tkinter import filedialog
 from scapy.packet import Packet
 from scapy.all import rdpcap
+from tkinter.filedialog import askopenfilename
+import os
 
 from ..data_buffer import DataBuffer
 from .. import modbus_util as mb
@@ -26,21 +28,27 @@ class Loader:
 
 
     def select_pcap_file(self):
+
         # Open file dialog to select a pcap file
         self.buffer.put("pcap", "Opening PCAP file dialog...")
-        file_path = filedialog.askopenfilename(
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        presets_dir = os.path.join(BASE_DIR, "..", "..", "..", "assets", "captures")
+        file_path = askopenfilename(
+            initialdir=presets_dir,
             title="Select a pcap file",
             filetypes=[("PCAP files", "*.pcap *.pcapng"), ("All files", "*.*")]
         )
-        if file_path:
-            self.buffer.put("pcap", f"Selected file: {file_path}")
-        else:
+        if file_path == "":
             self.buffer.put("pcap", "No file selected")
+            return file_path
+        
+        self.buffer.put("pcap", f"Selected file: {file_path}")
         return file_path
 
 
+
     def open_pcap_file(self, file_path):
-        if not file_path:
+        if not file_path or file_path == "":
             return
         self.buffer.put("pcap", f"Loading packets from {file_path}...")
         self.packets = rdpcap(file_path)
