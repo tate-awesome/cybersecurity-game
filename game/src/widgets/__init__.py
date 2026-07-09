974de1cad0d2bc5070f7bfee655d0a0a28afccbb
Tate Abercrombie <tate.aber.4@example.com>
2026-07-09 09:01:23 -0600

    Add a general PanedWindow class

--- game/src/widgets/__init__.py
+++ game/src/widgets/__init__.py
@@ -2,6 +2,7 @@ from .frame_widgets.menu_bar import MenuBar
 from .frame_widgets.title_menu import TitleMenu
 from .frame_widgets.trifold import Trifold
 from .frame_widgets.bifold import Bifold
+from .frame_widgets.panes import Panes
 from .frame_widgets.scrollable import Scrollable
 
 from .forms.arp import ArpForm
---
+++ game/src/widgets/frame_widgets/panes.py
@@ -0,0 +1,45 @@
+from tkinter import PanedWindow
+from customtkinter import CTkFrame
+from ...app_core.context import Context
+
+class Panes(PanedWindow):
+
+    def __init__(self, master, context: Context, direction = "horizontal", child_count: int = 3, child_sizes: list[int] = [4, 3, 2], pad_around = True):
+
+        if not direction in ["horizontal", "vertical"] or child_count < 2:
+            return
+
+        self.context = context
+        style = context.style
+
+        color = style.color("root")
+
+        super().__init__(master, orient=direction, background=color, sashwidth=style.igap, opaqueresize=0)
+
+        if pad_around:
+            self.pack(style.packing())
+        else:
+            self.pack(style.packing("panel"))
+        
+        master.update_idletasks()
+
+        if direction == "horizontal":
+            s = self.winfo_width() / context.style.get_scale_correction()
+        else:
+            s = self.winfo_height() / context.style.get_scale_correction()
+
+        self.panes = []       
+        for i in range(child_count):
+            self.panes.append(CTkFrame(self, height = s//child_sizes[i], width= s//child_sizes[i], background_corner_colors=(color, color, color, color)))
+            self.add(self.panes[i], minsize=style.PANE_MIN)
+
+        self.bind("<Configure>", self.on_pane_resize)
+
+    def on_pane_resize(self, event=None):
+        self.context.root.update_idletasks()
+    
+    def panes(self):
+        return self.panes
+
+    def pane(self, index: int):
+        return self.panes[index]
