06610acce640d4f7aef16ee82a1c7e2c54589187
Tate Abercrombie <tate.aber.4@example.com>
2026-07-09 08:36:28 -0600

    Fix responsiveness when resizing PanedWindows (Trifold, Bifold)

--- game/src/widgets/frame_widgets/bifold.py
+++ game/src/widgets/frame_widgets/bifold.py
@@ -6,13 +6,14 @@ from ...app_core.context import Context
 class Bifold(PanedWindow):
 
     def __init__(self, master, context: Context):
+        self.context = context
         style = context.style
 
         # Get root color
         rc = style.color("root")
 
         # Create paned window
-        super().__init__(master, orient="vertical", background=rc, sashwidth=style.igap)
+        super().__init__(master, orient="vertical", background=rc, sashwidth=style.igap, opaqueresize=0)
         self.pack(side="top", fill="both", expand=True, padx=style.nogap, pady=style.nogap)
 
         # Create panes with matching corners and preset widths
@@ -24,3 +25,8 @@ class Bifold(PanedWindow):
         # Add panes
         self.add(self.top, minsize=style.PANE_MIN)
         self.add(self.bottom, minsize=style.PANE_MIN)
+
+        self.bind("<Configure>", self.on_pane_resize)
+
+    def on_pane_resize(self, event=None):
+        self.context.root.update_idletasks()
--- game/src/widgets/frame_widgets/trifold.py
+++ game/src/widgets/frame_widgets/trifold.py
@@ -6,13 +6,14 @@ from ...app_core.context import Context
 class Trifold(PanedWindow):
 
     def __init__(self, master, context: Context):
+        self.context = context
         style = context.style
 
         # Get root color
         rc = style.color("root")
 
         # Create paned window
-        super().__init__(master, orient="horizontal", background=rc, sashwidth=style.igap)
+        super().__init__(master, orient="horizontal", background=rc, sashwidth=style.igap, opaqueresize=0)
         self.pack(fill="both", expand=True, padx=style.gap, pady=style.gap)
 
         # Create panes with matching corners and preset widths
@@ -26,3 +27,8 @@ class Trifold(PanedWindow):
         self.add(self.left, minsize=style.PANE_MIN)
         self.add(self.middle, minsize=style.PANE_MIN)
         self.add(self.right, minsize=style.PANE_MIN)
+
+        self.bind("<Configure>", self.on_pane_resize)
+
+    def on_pane_resize(self, event=None):
+        self.context.root.update_idletasks()
