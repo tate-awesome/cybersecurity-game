1e3e9b28e69bf45f4b3ef56c0a06283785871e78
Tate Abercrombie <tate.abercrombie2017@gmail.com>
2026-09-08 16:31:26 -0600

    Fix a migration bug where getting an entry would fail

--- game/src/widgets/panels/network_action_panel/forms/arp.py
+++ game/src/widgets/panels/network_action_panel/forms/arp.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLineEdit
 from .....app_core import Context
 from .....network.hardware import ArpSpoofer
 from ...base_form import BaseForm
@@ -12,6 +12,6 @@ class ArpForm(BaseForm):
 
         self.add_header()
 
-        label1, entry1 = self.add_labeled_entry("Target IP:")
-        label2, entry2 = self.add_labeled_entry("Host IP:")
-        self.add_process_row(lambda: self.process.start(entry1.get(), entry2.get()), self.process.stop, self.process.is_running)
+        _, entry1 = self.add_labeled_entry("Target IP:")
+        _, entry2 = self.add_labeled_entry("Host IP:")
+        self.add_process_row(lambda: self.process.start(entry1.text(), entry2.text()), self.process.stop, self.process.is_running)
